3703bdda68f4a4387b3342005b58151ffb402fca
AndreySoroko15 <andrejsoroko@MacBook-Air-Andrej.local>
2026-04-02 02:52:34 +0300

    addedd: store and update for category, param and product parent

--- app/Services/ProductParentService.php
+++ app/Services/ProductParentService.php
@@ -2,13 +2,19 @@
 
 namespace App\Services;
 
+use App\Models\ProductParent;
+
 class ProductParentService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function store(array $data): ProductParent
     {
-        //
+        return ProductParent::create($data);
+    }
+
+    public static function update(ProductParent $product_parent,array $data): ProductParent
+    {
+        $product_parent->update($data);
+
+        return $product_parent->fresh();
     }
 }
